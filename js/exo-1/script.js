1f295a4b504ed67bfb62d0945709e21eeada4478
Julia-BARGOIN <julia.bargoin@tuta.io>
2020-09-25 14:43:06 +0200

    chore(JavaScript-evaluation): architecture update

---
+++ js/exo-1/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Grid Generator
+ * @constructor
+ * @param {*} xAxis 
+ * @param {*} yAxis 
+ */
+var GridGenerator = function GridGenerator(xAxis, yAxis) {
+  this.xAxis = xAxis;
+  this.yAxis = yAxis;
+};
+
+/**
+ * run
+ */
+GridGenerator.prototype.run = function () {
+  return this;
+}
