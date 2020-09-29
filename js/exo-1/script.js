ebd02152a6674b9cdbf4a1f825f63f8ea1d2fa47
Julia-BARGOIN <julia.bargoin@tuta.io>
2020-09-29 11:30:29 +0200

    feat(grid): generating an array in html

--- js/exo-1/script.js
+++ js/exo-1/script.js
@@ -3,17 +3,39 @@
 /**
  * Grid Generator
  * @constructor
- * @param {*} xAxis 
- * @param {*} yAxis 
+ * @param {number} xAxis 
+ * @param {number} yAxis 
  */
-var GridGenerator = function GridGenerator(xAxis, yAxis) {
+var GridGenerator = function gridGenerator(xAxis, yAxis) {
   this.xAxis = xAxis;
   this.yAxis = yAxis;
-};
-
-/**
- * run
- */
-GridGenerator.prototype.run = function () {
-  return this;
 }
+
+GridGenerator.prototype.renderGrid = function () {
+  var createTable = document.createElement("table");
+  
+  for (var i = 0; i < this.xAxis; i++) {
+    var tr = document.createElement("tr");
+    createTable.appendChild(tr);
+
+    for (var j = 0; j < this.yAxis; j++) {
+      var td = document.createElement("td");
+      td.style.border = 'solid 1px';
+      td.style.width = '20px';
+      td.style.height = '20px';
+      tr.appendChild(td);
+    }
+  }
+  document.querySelector("body").appendChild(createTable);
+}
+
+GridGenerator.prototype.run = function () {
+  this.renderGrid();
+  return
+}
+
+new GridGenerator(4, 4).run();
+
+
+
+
